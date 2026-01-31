a7828532688245144655323590c918fed9750778
Arni Magnusson <thisisarni@gmail.com>
2026-01-31 19:26:38 -0300

    Add qcTAF-package help page

---
+++ R/qcTAF-package.R
@@ -0,0 +1,12 @@
+#' @name qcTAF-package
+#'
+#' @aliases qcTAF
+#'
+#' @title TAF Checks
+#'
+#' @description
+#' Tools to check TAF analyses.
+#'
+#' @author Arni Magnusson and Colin Millar.
+
+"_PACKAGE"
