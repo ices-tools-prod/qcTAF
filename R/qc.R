f61724b20e7e2098d1c825eaf45cd271f01d5a42
Arni Magnusson <thisisarni@gmail.com>
2026-02-03 11:19:44 -0300

    Simplify qc()

--- R/qc.R
+++ R/qc.R
@@ -2,11 +2,7 @@
 #'
 #' Run all TAF requirement checks on one or more analyses.
 #'
-#' @param x directory containing a TAF analysis or a directory containing
-#'        multiple TAF analyses.
-#' @param short whether to show the TAF directory name in a short
-#'        \code{\link{basename}} format.
-#' @param quiet whether to suppress messages.
+#' @param analysis directory containing a TAF analysis.
 #'
 #' @return
 #' Logical vector indicating the result from each test.
@@ -20,50 +16,35 @@
 #'
 #' \code{\link{qc.data.bib.exists}} checks if \verb{DATA.bib} file exists.
 #'
+#' \code{\link{qc.software.bib.exists}} checks if \verb{DATA.bib} file exists.
+#'
 #' \code{\link{qcTAF-package}} gives an overview of the package.
 #'
 #' @examples
 #' \dontrun{
 #' # TAF analysis
 #' qc("rjm-347d")
-#'
-#' # Multiple TAF analyses
-#' qc("2026/wgnssk")
 #' }
+#'
+#' @importFrom stats setNames
 #'
 #' @export
 
-qc <- function(x, short=TRUE, quiet=FALSE)
+qc <- function(analysis=".")
 {
-  # Print warnings as they occur, rather than all at the end
-  owarn <- options(warn=1); on.exit(options(owarn))
+  # 1  Preamble
+  tests <- c("dir.exists",
+             "qc.boot.exists",
+             "qc.data.bib.exists",
+             "qc.software.bib.exists")
+  s <- setNames(rep(NA, length(tests)), tests)
 
-  if(qc.boot.exists(x))  # one TAF dir
-  {
-    s <- ""
-    if(s == "" && !dir.exists(x))
-      s <- "dir exists"
-    if(s == "" && !qc.boot.exists(x))
-      s <- "boot exists"
-    if(s == "" && !qc.data.bib.exists(x))
-      s <- "DATA.bib exists"
-  }
-  else if(dir.exists(x))  # directory containing many TAF analyses
-  {
-    subdirs <- dir(x, full.names=TRUE)[dir.exists(dir(x, full.names=TRUE))]
-    folders <- if(short) basename(subdirs) else subdirs
-    s <- rep(NA_character_, length(folders))
-    for(i in seq_along(folders))
-    {
-      if(!quiet) cat("[", i, "] ", folders[i], "\n", sep="")
-      s[i] <- qc(subdirs[i], short=short, quiet=quiet)
-    }
-    names(s) <- folders
-  }
-  else
-  {
-    stop("'x' must be an existing directory name")
-  }
+  # 2  Test
+  s[1] <- get(tests[1])(analysis)
+  s[2] <- get(tests[2])(analysis)
+  s[3] <- get(tests[3])(analysis)
+  s[4] <- get(tests[4])(analysis)
 
-  invisible(s)
+  # 3  Result
+  s
 }
